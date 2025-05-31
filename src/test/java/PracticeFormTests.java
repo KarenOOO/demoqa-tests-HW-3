c90788131ba1c71657d39fbe8200db9b0d001856
Oganesyan Karen <kar99-99@bk.ru>
2025-05-31 18:15:00 +0300

    Added Исправление поиска элемента

--- src/test/java/PracticeFormTests.java
+++ src/test/java/PracticeFormTests.java
@@ -14,8 +14,6 @@ public class PracticeFormTests {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
-        Configuration.timeout = 5000; // default 5 sec
     }
 
     @Test

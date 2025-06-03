1e6fbec45f55c2edf39644cb7c0e421fc7aa70df
Oganesyan Karen <kar99-99@bk.ru>
2025-06-03 22:15:40 +0300

    Update tests and pages

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -1,11 +1,9 @@
 package pages.components;
 
-import com.codeborne.selenide.ElementsCollection;
-import com.codeborne.selenide.SelenideElement;
-import static com.codeborne.selenide.Condition.*;
+import com.codeborne.selenide.*;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CalendarComponent {
 

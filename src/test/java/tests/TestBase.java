fd74a52cbaffbfe945c20cc1a101c53987853c98
Karen <kar99-99@bk.ru>
2025-08-13 00:15:35 +0300

    added allure

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,7 +1,15 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import helpers.WebAttach;
+import io.qameta.allure.Attachment;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+
+import static com.codeborne.selenide.impl.Cleanup.of;
+import static java.util.Map.of;
 
 public class TestBase {
 
@@ -12,5 +20,20 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.of(
+                "enableVNC", true,
+            "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+
+    }
+
+    @Attachment
+    void addAttachments() {
+        WebAttach.screenshotAs("Last screenshot");
+        WebAttach.pageSource();
+        WebAttach.browserConsoleLogs();
     }
 }

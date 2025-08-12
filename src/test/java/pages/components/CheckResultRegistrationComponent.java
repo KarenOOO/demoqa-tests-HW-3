1707eaa62130f85a815041cfeae42e7930ca6bc0
Karen <kar99-99@bk.ru>
2025-08-12 19:09:56 +0300

    added allure

--- src/test/java/pages/components/CheckResultRegistrationComponent.java
+++ src/test/java/pages/components/CheckResultRegistrationComponent.java
@@ -1,6 +1,7 @@
 package pages.components;
 
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
@@ -8,9 +9,11 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class CheckResultRegistrationComponent {
 
+
     private final SelenideElement checkStudentRegistration = $(".table-responsive"),
             checkUserForm = $("#userForm");
 
+    @Step("Проверка заполненных полей")
     public CheckResultRegistrationComponent checkResult(String key, String value) {
         checkStudentRegistration.$(byText(key)).parent()
                 .shouldHave(text(value));

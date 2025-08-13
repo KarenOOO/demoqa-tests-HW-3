a86dea116d6dbf069690413da42be198e4dc9c2e
Karen <kar99-99@bk.ru>
2025-08-13 20:42:13 +0300

    added config

--- src/test/java/config/WebConfig.java
+++ src/test/java/config/WebConfig.java
@@ -25,7 +25,7 @@ public interface WebConfig extends Config {
     String getBaseUrl();
 
     @Key("remoteUrl")
-    @DefaultValue("https://user1:1234@selenoid.autotests.cloud/wd/hub")
+    @DefaultValue("https://selenoid.autotests.cloud/wd/hub")
     URL getRemoteUrl();
 
     @Key("isRemote")
